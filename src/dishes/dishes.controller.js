e42a0cf5d200b341cdc03fef6719987fa5f4fab6
Mculler25 <mdculler2@gmail.com>
2023-06-26 13:07:48 -0700

    added list and create functions to /dishes

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -7,3 +7,42 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /dishes handlers needed to make the tests pass
+const validateDataExists = (req, res, next) => {
+    if(req.body.data){
+        next()
+    } else {
+        next({
+            status : 400,
+            message : 'Request must contain a data object'
+        })
+    }
+}
+
+const validater = (field) => {
+    return function (req, res, next) {
+        if(req.body.data[field]){
+            next();
+        } else {
+            next({
+                status: 400,
+                message : `Please provide ${field} field`
+            })
+        }
+    }
+}
+const list = (req, res, next) => {
+    res.json({data : dishes})
+}
+
+const create = (req, res, next) => {
+
+}
+
+module.exports = {
+    list,
+    create : [
+        validateDataExists, 
+        ['name', 'description', 'price', 'image-url'].map(validater),
+        create
+    ]
+}
--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -1,6 +1,11 @@
+const methodNotAllowed = require('../utils/methodNotAllowed');
 const router = require("express").Router();
+const controller = require('./dishes.controller');
 
 // TODO: Implement the /dishes routes needed to make the tests pass
-
+router.route("/")
+    .get(controller.list)
+    .post(controller.create)
+    .all(methodNotAllowed);
 
 module.exports = router;
